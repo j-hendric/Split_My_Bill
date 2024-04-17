4d4c18811363b4fb7df3ab0d941103848d976f03
Jenna Hendrickson <jennahendrickson83@gmail.com>
2024-04-16 20:01:51 -0600

    local storage updates

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SplitMyBill;
@@ -8,4 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
